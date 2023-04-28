92deeffc56f3fc96a5b7c0296aff0440a84ff31a
Khalid Saifullah <khalideofficial@gmail.com>
2023-04-28 07:53:40 +0600

    new update

--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -3,7 +3,7 @@ export const main = {
     "occupation": "Founder of KS Devware",
     "description": "I'm Software Engineer",
     "image": "profilepic.jpg",
-    "bio": "As the founder of KS Devware (software development company), I am Khalid Saifullah. A young entrepreneur. With nearly 4 years of experience, I’m a software engineer (full-stack web & mobile) and UI/UX designer. Worked as a senior lead full-stack developer at OpticEngine had been a rewarding experience. Moreover, I work on a few startups aimed at the Bangladesh, Russian, Chinese, American, and Dutch markets. Among the companies and entrepreneurs I have worked with are DP-Websolutions, Ganlax group, Hyvinvoivin, Happy-Dairy, Arrtal, FN Softwares & institute, and many more. Developing software for the world of intent is something I'm passionate about and I'm dedicated to making the world of intent easy and affordable for everyone.",
+    "bio": "As the founder of KS Devware, WorksLee, Pocotep, and GoreLove, and the co-founder and CTO of Arrtal and Adatun, I have proven experience building and leading successful companies. My technical expertise, leadership skills, and entrepreneurial spirit have enabled me to create innovative products and services that meet the needs of my customers. I am driven by a passion for problem-solving and a desire to positively impact the world through my work. As a Full-stack JavaScript developer, UI/UX designer, mobile app developer, and digital marketer with over 5 years of experience, I have a diverse set of skills that make me well-suited to tackle a wide range of projects. My expertise in JavaScript and its frameworks, such as React and Node, enables me to build dynamic and responsive websites and applications. My front-end skills in HTML, CSS, and React allow me to create visually appealing and user-friendly interfaces. My experience with back-end technologies like Express and REST API enables me to connect my web and mobile applications to databases and other resources. In addition to my technical skills, I have a strong understanding of design principles and user experience, as demonstrated by my experience with libraries like Tailwind CSS, Bootstrap, and Material UI. My ability to use Figma and other design tools allows me to create mockups and prototypes that effectively communicate my ideas. My familiarity with payment systems like Stripe, and PayPal will enable me to integrate financial transactions into my projects. I have extensive leadership experience and am comfortable managing a team of 10 engineers and designers. As a startup founder and someone who has worked with multiple companies throughout my career, I have a track record of helping businesses grow and succeed. I am also a coding instructor, demonstrating my ability to effectively communicate technical concepts and mentor others in their development journey. My familiarity with cloud platforms like Google, AWS, Digital Ocean, Heroku, and Vercel enables me to deploy and host my applications and websites in a scalable and reliable manner. My experience with blockchain and web3.js allows me to explore the potential of decentralized applications and incorporate them into my work. My proficiency with Git and GitHub enables me to collaborate with other developers and manage version control in my projects. My knowledge of PWA (Progressive Web Apps) allows me to build applications that are fast, reliable, and work offline, giving users a seamless experience on any device. Finally, I'm so cool.",
     "email": "khalideofficial@gmail.com",
     "phone": "+880 17874 63481",
     "quote": "The universe is a pretty big place. If it's just us.",
@@ -45,9 +45,9 @@ export const main = {
 }
 
 export const about = {
-    "bio": "I am a software <br /><span class='text-primary'>engineer</span> who cares<br /> about the code and the people.",
-    "ido": "<p>WEB DEVELOPMENT</p><p>APP DEVELOPMENT</p><p>UI/UX DESIGN</p><p>FRONT-END DEVELOPMENT</p><p>BACK-END DEVELOPMENT</p><p>FULL-STACK DEVELOPMENT</p>",
-    "years": 4,
+    "bio": "I'm a <span class='text-primary'>software</span> <u>engineer</u> turned <span class='text-primary'>entrepreneur</span> who creates innovative solutions and builds <span class='text-primary'>successful</span> <u>businesses</u>",
+    "ido": "<p>• WEB DEVELOPMENT</p><p>• APP DEVELOPMENT</p><p>• UI/UX DESIGN</p><p>• FULL-STACK DEVELOPMENT</p><p>• DIGITAL MARKETING</p><p>• ENTREPRENEURSHIP</p>",
+    "years": 5,
     "image": "img/profilepic.jpg",
     // "video": "https://www.youtube.com/watch?v=ghQd1PbzVaY",
     "resumedownload": "https://drive.google.com/file/d/1uJ9B1U15GI5jPZfzen3Kk8X5HUgG_qDG/view?usp=sharing",
@@ -57,18 +57,18 @@ export const works = [
     [
         
         {
-            "company": "DP-Websolutions",
-            "title": "Full-Stack Developer & UI/UX designer",
+            "company": "Health Hero, Inc",
+            "title": "Senior Frontend Developer & UI/UX Designer",
             "image": "img/behance.png",
-            "years": "July 2020 - Present",
-            "description": "• Dedicated years of collaborative effort resulted in 30% increase in company revenue <br />• Responsible for testing applications and looking for bugs & monitoring website performance <br />• Responsible for application code and UI/UX design <br />• Built responsive custom WordPress themes from scratch also with DIVI theme <br />• Optimized company website for performance and Search Engine Optimization"
+            "years": "Sep 2022 - Present",
+            "description": "• Worked closely with the CTO and improved the core products.<br />• Re-designed and rebuilt the main web app. Now that app tracks 2.5 Million steps every week. Also, user engagement has increased by 35%.<br />• Worked on HelthHero metaverse land, Improved onboarding experience by collaborating with a UX designer, built a corporate land, and also worked with blockchain and NFT stuff on the Polygon network.<br />• Rebuilt the W-NFT Minting process, during this project, I worked very closely with the smart contract and NFT.<br />• Re-designed the current landing page of the company. That has added more value and increased visitors.<br />• Helped on increasing user experience by brainstorming and collaborating with the core team.<br />"
         },
         {
-            "company": "OpticEngine",
-            "title": "Senior Lead Full-Stack Web & Mobile Developer",
+            "company": "Egoo Technologies LLC",
+            "title": "Senior App Developer",
             "image": "img/behance.png",
-            "years": "June 2021 - October 21",
-            "description": "• Built websites with React JS and node js as backend <br />• Designing web and mobile apps mockups using Figma <br />• Designed and built the User Interface of the mobile app with React-native <br />• Assist the backend developer on ongoing development projects <br />• Participating in all decision-making processes which includes the database design, tooling, deployment processes etc"
+            "years": "Dec 2022 - Present",
+            "description": "I have been responsible for the design and development of various projects. My primary focus has been on rebuilding the Rider and Driver app using React Native, with a focus on best practices in order to improve the overall user interface and user experience. Additionally, I have designed and developed an admin portal and a branded landing page for the application. Furthermore, I have implemented a serverless backend infrastructure to support the application."
         },
         {
             "company": "FN Softwares & institute",
@@ -92,6 +92,13 @@ export const works = [
             "image": "img/behance.png",
             "years": "March 2021 - Present",
             "description": "• Developing revenue-generating applications at both small and large scales <br />• Providing comprehensive maintenance support <br />• Improving the user interface <br />• Building apps for self-health and helping the nation"
+        },
+        {
+            "company": "OpticEngine",
+            "title": "Senior Lead Full-Stack Web & Mobile Developer",
+            "image": "img/behance.png",
+            "years": "June 2021 - October 21",
+            "description": "• Built websites with React JS and node js as backend <br />• Designing web and mobile apps mockups using Figma <br />• Designed and built the User Interface of the mobile app with React-native <br />• Assist the backend developer on ongoing development projects <br />• Participating in all decision-making processes which includes the database design, tooling, deployment processes etc"
         },
     ]
 ]
@@ -149,6 +156,18 @@ export const projects = [
         "category": "Website",
         "image": "business2.png",
         "url": "https://megakit-web.netlify.app"
+    },
+    {
+        "title": "GanlaxGroup.co.uk",
+        "category": "Website",
+        "image": "ganlax.jpg",
+        "url": "http://ganlaxgroup.co.uk"
+    },
+    {
+        "title": "Daily-dairy.com",
+        "category": "Website",
+        "image": "daily-dairy.jpg",
+        "url": "https://daily-dairy.com"
     },
     {
         "title": "Zeppto website",
@@ -240,24 +259,7 @@ export const projects = [
         "image": "amazon.jpg",
         "url": "https://pocotep.com"
     },
-    {
-        "title": "Hyvinvoivin.fi",
-        "category": "Website",
-        "image": "hyvn.jpg",
-        "url": "https://hyvinvoivin.fi"
-    },
-    {
-        "title": "GanlaxGroup.co.uk",
-        "category": "Website",
-        "image": "ganlax.jpg",
-        "url": "http://ganlaxgroup.co.uk"
-    },
-    {
-        "title": "Daily-dairy.com",
-        "category": "Website",
-        "image": "daily-dairy.jpg",
-        "url": "https://daily-dairy.com"
-    },
+
     {
         "title": "KhalidDev.com",
         "category": "Website",
